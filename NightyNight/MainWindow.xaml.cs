be7b37e27d8bf938325cca25440d8327064725f2
Jonathan Fraimorice <yonifra@gmail.com>
2015-03-04 15:24:52 +0200

    First commit of Nighty Night
    Allows automatic shutdown of the PC given the amount of minutes.
    Allows abort of the automatic shutdown process.

---
+++ NightyNight/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace NightyNight
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            int timeInMinutes = 45;
+            
+            var time = int.TryParse(ShutdownTimeTextBox.Text, out timeInMinutes) ? (timeInMinutes * 60).ToString() : "2700";
+
+            Process.Start("shutdown.exe", "-s -t " + time);
+        }
+
+        private void AbortButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("shutdown.exe", "-a");
+        }
+    }
+}
